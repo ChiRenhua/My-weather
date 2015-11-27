e4f27955ee1f3d9c6b9d1ae8973817513a1adc01
lgy <lgy@amyxfliutekiimac.local>
2015-11-27 15:07:17 +0800

    Initial Commit

---
+++ My weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My weather
+//
+//  Created by lgy on 15/11/27.
+//  Copyright © 2015年 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My weather
+//
+//  Created by lgy on 15/11/27.
+//  Copyright © 2015年 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
